d4a5ff4454ae50e41bd5720c38b19a985303ef1a
MindBlowingNoob <rajatkashyap099@gmail.com>
2022-07-17 13:08:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [size, setSize] = useState({height:300, width: 300});
+  function handleClick(prev){
+    setSize(prev => {
+      return {
+        height: prev.height+2,
+        width: prev.width+2
+      }
+    })
+  }
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img src={star}  height={`${size.height}px`} width={`${size.width}px`} onClick={handleClick}/>
     </div>
   )
 }
